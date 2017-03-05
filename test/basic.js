b091df50a534348cc9dccb915f66bc2a1ad15afb
Matthias ETIENNE <matt@allty.com>
2017-03-06 00:38:48 +0100

    test: Added test for comparison

--- test/basic.js
+++ test/basic.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 const micromist = require('../');
+const minimist = require('minimist');
 
 const args = micromist(process.argv);
 
+console.log("micromist");
 console.log(args);
 
+
+const args2 = minimist(process.argv.slice(2));
+
+console.log("minimist");
+console.log(args2);
