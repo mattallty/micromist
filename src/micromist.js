a16dca466d1218056696baae6f394f64c81fa1d9
Matthias ETIENNE <matt@allty.com>
2017-03-06 00:27:19 +0100

    fix: fix nasty bug and make it faster
    Last argument/option was sometimes swallowed

--- src/micromist.js
+++ src/micromist.js
@@ -2,45 +2,45 @@
 
 const camelCase = require('lodash.camelcase');
 
+function isOption(str) {
+  return (str || '').match(/^--?[^\d]+/);
+}
+
+function isMulti(str) {
+  if (str.match(/^-([a-z]{2,})/i)) {
+    return str.substr(1).split('');
+  }
+}
+
+function compute(r, name, val) {
+  let nameCam = camelCase(name.replace(/^--?/, ''));
+  if (name.substr(0, 5) === '--no-') {
+    nameCam = camelCase(nameCam.substr(2));
+    val = false;
+  }
+  const mul = isMulti(name);
+  if (mul) {
+    mul.forEach(o => r[o] = true);
+    return r;
+  }
+  r[nameCam] = nameCam in r ? [r[nameCam]].concat(val) : val;
+  return r;
+}
+
 function micromist(args) {
 
   args = args.slice(2);
 
   const r = {_:[]};
-  const it = args[Symbol.iterator]();
-  let curr;
+  let pointer = 0;
 
-  function isOption(str) {
-    return (str || '').match(/^--?[^\d]+/);
-  }
+  while(typeof args[pointer] !== 'undefined') {
 
-  function isMulti(str) {
-    if (str.match(/^-([a-z]{2,})/i)) {
-      return str.substr(1).split('');
-    }
-  }
-
-  function compute(r, name, val) {
-    let nameCam = camelCase(name.replace(/^--?/, ''));
-    if (name.substr(0, 5) === '--no-') {
-      nameCam = camelCase(nameCam.substr(2));
-      val = false;
-    }
-    const mul = isMulti(name);
-    if (mul) {
-      mul.forEach(o => r[o] = true);
-      return r;
-    }
-    r[nameCam] = nameCam in r ? [r[nameCam]].concat(val) : val;
-    return r;
-  }
-
-  while((curr = it.next()) && !curr.done) {
-
-    const val = curr.value;
+    const val = args[pointer];
 
     if (!isOption(val)) {
       r._.push(val);
+      pointer++;
       continue;
     } else if (val === '--') {
       r._ = r._.concat(args.slice(args.indexOf('--')));
@@ -48,13 +48,17 @@ function micromist(args) {
     }
 
     const parts = val.split('=');
+    let isNextOpt = true;
 
     if (parts.length > 1) {
       compute(r, parts[0], parts[1]);
     } else {
-      const next = it.next();
-      compute(r, parts[0], isOption(next.value) ? true : next.value || true);
+      const next = args[pointer+1];
+      isNextOpt = isOption(next);
+      compute(r, parts[0], isNextOpt ? true : next || true);
     }
+
+    pointer += isNextOpt ? 1 : 2;
   }
 
   return r;
